57920e346e29d12e529b980592d9224c4c73b846
Ismaelgz <59649269+Ismaelgz@users.noreply.github.com>
2020-01-28 11:54:29 +0100

    Update Cliente.java

--- Tienda moviles/src/Cliente.java
+++ Tienda moviles/src/Cliente.java
@@ -1,4 +1,33 @@
 
 public class Cliente {
 
+  private String CIF, datosPersonales, cuentaCorriente;
+
+	
+	public Cliente(String cIF, String dp, String ncc) {
+		super();
+
+		CIF = cIF;
+		this.datosPersonales = dp;
+		this.cuentaCorriente = ncc;
+	}
+
+	public String getCuentaCorriente() {
+		return cuentaCorriente;
+	}
+
+	public void setCuentaCorriente(String cuentaCorriente) {
+		this.cuentaCorriente = cuentaCorriente;
+	}
+
+	public String getCIF() {
+		return CIF;
+	}
+
+	public String getDatosPersonales() {
+		return datosPersonales;
+	}
+  
+  
+  
 }
