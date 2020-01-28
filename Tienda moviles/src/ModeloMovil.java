2894ee1d93ea47b8b4e9427f17822ee69e45d288
venancio98 <59649505+venancio98@users.noreply.github.com>
2020-01-28 12:05:27 +0100

    Modelos moviles

--- Tienda moviles/src/ModeloMovil.java
+++ Tienda moviles/src/ModeloMovil.java
@@ -1,4 +1,41 @@
 
+// POJO
 public class ModeloMovil {
+	private String codigoModelo, caracteristicas;
+	private float precio;
+	
+	private Estado estado;
+	
+	public ModeloMovil(String codigoModelo, String caracteristicas, float precio) {
+		super();
+		this.codigoModelo = codigoModelo;
+		this.caracteristicas = caracteristicas;
+		this.precio = precio;
+		estado=Estado.produciendose;
+	}
 
+	public float getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	public String getCodigoModelo() {
+		return codigoModelo;
+	}
+
+	public String getCaracteristicas() {
+		return caracteristicas;
+	}
+	
 }
