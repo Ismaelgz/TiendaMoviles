12bac2add340eb98e2389811a641ae42ceab614c
javierpl99 <59649522+javierpl99@users.noreply.github.com>
2020-01-28 12:05:46 +0100

    Móvil

--- Tienda moviles/src/Movil.java
+++ Tienda moviles/src/Movil.java
@@ -1,4 +1,29 @@
 
 public class Movil {
+	private String codReferencia;
+	private Color color;
+	private ModeloMovil modelo;
+
+
+	public Movil(String codReferencia, Color color, ModeloMovil modelo) {
+		super();
+		this.codReferencia = codReferencia;
+		this.color = color;
+		this.modelo = modelo;
+	}
+
+
+	public String getCodReferencia() {
+		return codReferencia;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public ModeloMovil getModelo() {
+		return modelo;
+	}
 
 }
+
