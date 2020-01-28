d7327ca526a7eb51c1bb0dd25f8eb059707b2357
Ismaelgz <59649269+Ismaelgz@users.noreply.github.com>
2020-01-28 12:00:25 +0100

    Fecha

--- Tienda moviles/src/Fecha.java
+++ Tienda moviles/src/Fecha.java
@@ -1,4 +1,31 @@
 
 public class Fecha {
-
+	private int dia;
+	private int mes;
+	private int anno;
+	public Fecha(int dia, int mes, int anno) {
+		super();
+		this.dia = dia;
+		this.mes = mes;
+		this.anno = anno;
+	}
+	public int getDia() {
+		return dia;
+	}
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+	public int getMes() {
+		return mes;
+	}
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+	public int getAnno() {
+		return anno;
+	}
+	public void setAnno(int anno) {
+		this.anno = anno;
+	}
+	
 }
