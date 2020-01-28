cf34044b4ea0a1b09533b0a9e9a63d97c8782d99
Ismaelgz <59649269+Ismaelgz@users.noreply.github.com>
2020-01-28 12:09:18 +0100

    Venta

--- Tienda moviles/src/Venta.java
+++ Tienda moviles/src/Venta.java
@@ -1,4 +1,15 @@
 
 public class Venta {
+	private Movil movil;
+	private Cliente cliente;
+	private String fechaVenta;
+
+	public Venta(Movil movil, Cliente cliente, String fechaVenta) {
+		super();
+		this.movil = movil;
+		this.cliente = cliente;
+
+		this.fechaVenta = fechaVenta;
+	}
 
 }
